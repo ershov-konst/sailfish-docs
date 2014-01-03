3f99bea7584410dcf5fde0aa7739ddff391dd71c
ershov-konst <ershov.konst@gmail.com>
2014-01-03 22:12:48 +0400

    auto documentation fot client modules

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,25 @@ var
    Component = require("sailfish").Component,
    marked = require("marked"),
    path = require("path"),
-   fs = require("fs");
+   fs = require("fs"),
+   docs = require('../lib/docs'),
+   dot = require('dot'),
+   tplPath = path.resolve(__dirname, '../lib'),
+   dotDef = {
+      include: function(tpl){
+         var result;
+
+         try{
+            result = fs.readFileSync(path.join(tplPath, tpl), 'utf8');
+         }
+         catch(e){
+            result = e.message;
+         }
+         return result;
+      }
+   };
+
+dot.templateSettings.strip = false;
 
 /**
  * returns html
@@ -10,14 +28,38 @@ var
  * @param cb
  */
 function getMarkup(url, cb){
-   fs.readFile("./source/" + url + ".md", "utf8", function(e, text){
-      if (e){
-         cb(e);
-      }
-      else{
-         cb(null, marked(text));
-      }
-   });
+   if (/api\/classes/.test(url)){
+      docs('./node_modules/sailfish/sf_client/lib', function(err, docObject){
+         fs.readFile(path.join(tplPath, 'docs.dot'), 'utf8', function(err, template){
+            if (!err){
+               var
+                  dotTplFn,
+                  txt;
+               try{
+                  dotTplFn = dot.template(template, null, dotDef);
+                  txt = dotTplFn(docObject);
+                  cb(null, marked(txt));
+               }
+               catch (e){
+                  cb(null, e.message);
+               }
+            }
+            else{
+               cb(null, err.message);
+            }
+         });
+      });
+   }
+   else{
+      fs.readFile("./source/" + url + ".md", "utf8", function(e, text){
+         if (e){
+            cb(e);
+         }
+         else{
+            cb(null, marked(text));
+         }
+      });
+   }
 }
 
 function sendRes(req, res){
